55a78ced1f08129532a49dee8a58ba6a22fae55b
Philipp Avans <Floppyrip@gmail.com>
2019-12-09 20:18:15 +0300

    Updates

---
+++ precache-manifest.974a6a54f8645ab3e7c56f9cf6b4f135.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8e0282629282850996eddfed7c44f0",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "eb1c78fad2295fa4a6df",
+    "url": "/reactApp/static/css/main.bf5ed4ff.chunk.css"
+  },
+  {
+    "revision": "0c25ade3445a27ba5259",
+    "url": "/reactApp/static/js/2.4ef70fe2.chunk.js"
+  },
+  {
+    "revision": "eb1c78fad2295fa4a6df",
+    "url": "/reactApp/static/js/main.a210fc40.chunk.js"
+  },
+  {
+    "revision": "07b5dfcf019dc9135575",
+    "url": "/reactApp/static/js/runtime-main.c26bb5e5.js"
+  },
+  {
+    "revision": "188c8180fad020fec32cdd2c783a9bd9",
+    "url": "/reactApp/static/media/boat.188c8180.svg"
+  },
+  {
+    "revision": "4ab1759379d9bd8c9998f897a9b15cee",
+    "url": "/reactApp/static/media/cloud1.4ab17593.svg"
+  },
+  {
+    "revision": "2d02e2236671bd83fd3b8fa112540acf",
+    "url": "/reactApp/static/media/cloud2.2d02e223.svg"
+  },
+  {
+    "revision": "5de92f1f7b896414b8236cbf125353ec",
+    "url": "/reactApp/static/media/cloud3.5de92f1f.svg"
+  },
+  {
+    "revision": "8e5ac0eaca35d68cea2ec70b6ac74d35",
+    "url": "/reactApp/static/media/customAvatar.8e5ac0ea.jpg"
+  },
+  {
+    "revision": "8339f1bf7010e0452428cccd0d3a9a2c",
+    "url": "/reactApp/static/media/moon.8339f1bf.svg"
+  },
+  {
+    "revision": "4c2164528f7551a29111102aa1711411",
+    "url": "/reactApp/static/media/wave1.4c216452.svg"
+  },
+  {
+    "revision": "690d494834a18e10a9c476350f935b39",
+    "url": "/reactApp/static/media/wave2.690d4948.svg"
+  },
+  {
+    "revision": "1f6cf712c1e467998354577e20735ca4",
+    "url": "/reactApp/static/media/wave3.1f6cf712.svg"
+  },
+  {
+    "revision": "babc32efb8efab14ab315dae9f3c2cb7",
+    "url": "/reactApp/static/media/wave4.babc32ef.svg"
+  }
+]);
